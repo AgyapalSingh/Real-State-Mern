c6e35b78412acc710a078049ce6db6891932ff16
Agyapal Singh <agyapalsinghaujla@gmail.com>
2024-11-04 17:24:37 +0530

    Sign Up Page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <Layout title='Sign Up | AujlaEstate'>
-      <div>Sign Up</div>
-    </Layout>
-  )
-}
+    <Layout title="Sign Up | AujlaEstate">
+      <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
 
-export default SignUp
+        <form className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="username"
+            className="border p-3 rounded-lg"
+            id="username"
+          />
+
+          <input
+            type="email"
+            placeholder="email"
+            className="border p-3 rounded-lg"
+            id="username"
+          />
+
+          <input
+            type="password"
+            placeholder="password"
+            className="border p-3 rounded-lg"
+            id="username"
+          />
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Sign Up
+          </button>
+        </form>
+
+
+        <div className="flex gap-2 mt-5">
+          <p>Have an account</p>
+          <Link to={"/sign-in"}>
+            <span className="text-blue-700">Sign In</span>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default SignUp;
