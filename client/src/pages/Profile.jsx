40623437dca62eb196a83397e5104424e9ec3cf7
Agyapal Singh <agyapalsinghaujla@gmail.com>
2024-11-07 16:32:15 +0530

    Complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -138,6 +138,25 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Layout title="Profile  | AujlaEstate">
       <div className="p-3 max-w-lg mx-auto">
