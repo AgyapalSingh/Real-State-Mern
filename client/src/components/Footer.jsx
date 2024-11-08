3906b482dcfd3984ab517cb276b7f71878a86e92
Agyapal Singh <agyapalsinghaujla@gmail.com>
2024-11-08 17:04:14 +0530

    Footer completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='bg-slate-200 shadow-md p-10 flex flex-col justify-center items-center'>
+      <span className='text-sm'>© 2024 Uniqaya Lifestyle | All Rights Reserved</span>
+      <span className='font-semibold p-3'>Created By AGYAPAL SINGH using MERN STACK and Tailwind CSS</span>
+    </footer>
   )
 }
